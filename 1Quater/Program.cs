5c2a8dfcbc5f16d8a8b9dd573c50c86e0b7809bf
IgorRoma <romigole@gmail.com>
2022-09-10 11:44:34 +0300

    array output

--- 1Quater/Program.cs
+++ 1Quater/Program.cs
@@ -2,3 +2,14 @@
 
 string[] arrayFormed =  {"Task", "Hi", "distance", "privet", "thi", "-333", "s", "1989",
                         "my_sun", "is", "abcd", "Greece", "my", "fw", "China", "Belarus"};
+
+void PrintArray(string[] array) 
+    {
+
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+                else Console.Write($"{array[i]}.");
+            }
+        Console.WriteLine();
+    }
