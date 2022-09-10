94c5c3e1474c2528d98cfaec8f3167a8f51f4eb4
IgorRoma <romigole@gmail.com>
2022-09-10 11:37:19 +0300

    start

---
+++ 1Quater/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+
+string[] arrayFormed =  {"Task", "Hi", "distance", "privet", "thi", "-333", "s", "1989",
+                        "my_sun", "is", "abcd", "Greece", "my", "fw", "China", "Belarus"};
